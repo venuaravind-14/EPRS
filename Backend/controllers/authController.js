41cc92c4804180ceeb0cf3d915a2997635fa1145
venuaravind-14 <venu14092006@gmail.com>
2026-04-30 02:42:47 +0530

    Extend JWT expiration to 30 days

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -79,7 +79,7 @@ exports.login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, username: user.username, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: '10d' }
+      { expiresIn: '30d' }
     );
 
     let roleDetails = {};
